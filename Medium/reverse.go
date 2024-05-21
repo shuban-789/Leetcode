93e9677d020ecc94308668e06ca4fcf5117c11cf
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-05-20 19:30:34 -0700

    Create reverse.go

---
+++ Medium/reverse.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func reverse(x int) int {
+	var xrev []string
+	var negative bool
+	if x < 0 {
+		negative = true
+		x = -x
+	} else {
+		negative = false
+	}
+	xstring := strconv.Itoa(x)
+	for i := len(xstring) - 1; i >= 0; i-- {
+		xrev = append(xrev, string(xstring[i]))
+	}
+	strxrev := strings.Join(xrev, "")
+	xrevint, err := strconv.Atoi(strxrev)
+	if err != nil {
+		panic(err)
+	}
+	if negative {
+		xrevint = -xrevint
+	}
+    if xrevint < -2147483648 || xrevint > 2147483648 {
+        return 0
+    }
+	return xrevint
+}
